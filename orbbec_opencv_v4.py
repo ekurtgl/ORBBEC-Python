54dcc21e0368b9a95c481d90536f28fc21e550b6
ekurtgl <66868163+ekurtgl@users.noreply.github.com>
2022-03-30 19:14:14 -0500

    Add files via upload

--- orbbec_opencv_v4.py
+++ orbbec_opencv_v4.py
@@ -6,14 +6,6 @@ from openni import openni2
 from openni import _openni2 as c_api
 # python 2
 
-fps = 30
-width = 640
-height = 480
-mirroring = True
-compression = False
-# duration = 3  # in sec
-# fname = 'orbbec'
-
 # Create the parser
 parser = argparse.ArgumentParser()
 
@@ -24,74 +16,100 @@ parser.add_argument('--duration', type=int, required=True)
 # Parse the argument
 args = parser.parse_args()
 
-# Initialize the depth device
-openni2.initialize()
-dev = openni2.Device.open_any()
-if dev.has_sensor(openni2.SENSOR_COLOR):
-    print("Color Stream found")
 
-# Start the depth stream
-depth_stream = dev.create_depth_stream()
-depth_stream.set_mirroring_enabled(mirroring)
-dev.set_image_registration_mode(True)
-dev.set_depth_color_sync_enabled(True)
-depth_stream.set_video_mode(c_api.OniVideoMode(pixelFormat=c_api.OniPixelFormat.ONI_PIXEL_FORMAT_DEPTH_100_UM,
-                                               resolutionX=width, resolutionY=height, fps=fps))
-depth_stream.start()
+def main():
+    fps = 30
+    width = 640
+    height = 480
+    mirroring = True
+    compression = False
+    # duration = 3  # in sec
+    # fname = 'orbbec'
 
-# Start the color stream
-is_color_available = dev.has_sensor(c_api.OniPixelFormat.ONI_PIXEL_FORMAT_RGB888)
-print('is_color_available:', is_color_available)
-# color_stream = dev.create_color_stream()
-# color_stream.set_mirroring_enabled(mirroring)
-# color_stream.start()
-# color_stream.set_video_mode(c_api.OniVideoMode(pixelFormat=c_api.OniPixelFormat.ONI_PIXEL_FORMAT_RGB888,
-#                                                resolutionX=width,
-#                                                resolutionY=height,
-#                                                fps=fps))
+    # Initialize the depth device
+    openni2.initialize()
+    dev = openni2.Device.open_any()
+    if dev.has_sensor(openni2.SENSOR_COLOR):
+        print("Color Stream found")
+    else:
+        print("No Color Stream found")
 
-# Function to return some pixel information when the OpenCV window is clicked
-refPt = []
-selecting = False
+    # Start the depth stream
+    depth_stream = dev.create_depth_stream()
+    depth_stream.set_mirroring_enabled(mirroring)
+    dev.set_image_registration_mode(True)
+    dev.set_depth_color_sync_enabled(True)
+    depth_stream.set_video_mode(c_api.OniVideoMode(pixelFormat=c_api.OniPixelFormat.ONI_PIXEL_FORMAT_DEPTH_100_UM,
+                                                   resolutionX=width, resolutionY=height, fps=fps))
+    depth_stream.start()
 
-# Initial OpenCV Window Functions
-cv2.namedWindow("Depth Image")
-# cv2.setMouseCallback("Depth Image", point_and_shoot)
+    # Start the color stream
+    is_color_available = dev.has_sensor(c_api.OniPixelFormat.ONI_PIXEL_FORMAT_RGB888)
+    print('is_color_available:', is_color_available)
+    # color_stream = dev.create_color_stream()
+    # color_stream.set_mirroring_enabled(mirroring)
+    # color_stream.start()
+    # color_stream.set_video_mode(c_api.OniVideoMode(pixelFormat=c_api.OniPixelFormat.ONI_PIXEL_FORMAT_RGB888,
+    #                                                resolutionX=width,
+    #                                                resolutionY=height,
+    #                                                fps=fps))
 
-rec = openni2.Recorder((args.filename + ".oni").encode('utf-8'))
-rec.attach(depth_stream, compression)
-# rec.attach(color_stream, compression)
-rec.start()
+    # Function to return some pixel information when the OpenCV window is clicked
+    # refPt = []
+    # selecting = False
 
-start = time.time()
-# Loop
-# while True:
-while time.time() - start < args.duration:
-    # Grab a new depth frame
-    frame = depth_stream.read_frame()
-    frame_data = frame.get_buffer_as_uint16()
-    # Put the depth frame into a numpy array and reshape it
-    img = np.frombuffer(frame_data, dtype=np.uint16)
-    img.shape = (1, 480, 640)
-    img = np.concatenate((img, img, img), axis=0)
-    img = np.swapaxes(img, 0, 2)
-    img = np.swapaxes(img, 0, 1)
+    # Initial OpenCV Window Functions
+    # cv2.namedWindow("Depth Image")
+    # cv2.setMouseCallback("Depth Image", point_and_shoot)
 
-    if len(refPt) > 1:
-        img = img.copy()
-        cv2.rectangle(img, refPt[0], refPt[1], (0, 255, 0), 2)
+    rec = openni2.Recorder((args.filename + ".oni").encode('utf-8'))
+    rec.attach(depth_stream, compression)
+    # rec.attach(color_stream, compression)
+    rec.start()
 
-    # Display the reshaped depth frame using OpenCV
-    cv2.imshow("Depth Image", img)
-    key = cv2.waitKey(1) & 0xFF
+    prev_time = 0
+    start = time.time()
+    print('Orbbec time: ' + str(prev_time))
 
-    # If the 'c' key is pressed, break the while loop
-    # if key == ord("c"):
-    #     break
+    while time.time() - start < args.duration:
+        if round(time.time() - start, 1) % 0.5 == 0 and round(time.time() - start, 1) != prev_time:
+            prev_time = round(time.time() - start, 1)
+            print('Orbbec time: ' + str(prev_time))
 
-# Close all windows and unload the depth device
-openni2.unload()
-# rec.stop()
-# depth_stream.stop()
-# color_stream.stop()
-cv2.destroyAllWindows()
+    # while True:
+    #     if time.time() - start >= args.duration:
+    #         break
+
+        # Grab a new depth frame
+        # frame = depth_stream.read_frame()
+        # frame_data = frame.get_buffer_as_uint16()
+        # # Put the depth frame into a numpy array and reshape it
+        # img = np.frombuffer(frame_data, dtype=np.uint16)
+        # img.shape = (1, 480, 640)
+        # img = np.concatenate((img, img, img), axis=0)
+        # img = np.swapaxes(img, 0, 2)
+        # img = np.swapaxes(img, 0, 1)
+
+        # if len(refPt) > 1:
+        #     img = img.copy()
+        #     cv2.rectangle(img, refPt[0], refPt[1], (0, 255, 0), 2)
+
+        # Display the reshaped depth frame using OpenCV
+        # cv2.imshow("Depth Image", img)
+        # key = cv2.waitKey(1) & 0xFF
+
+        # If the 'c' key is pressed, break the while loop
+        # if key == ord("c"):
+        #     break
+
+    # Close all windows and unload the depth device
+    openni2.unload()
+    # time.sleep(1)
+    # rec.stop()
+    # depth_stream.stop()
+    # color_stream.stop()
+    # cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
